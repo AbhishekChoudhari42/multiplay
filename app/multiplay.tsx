0479802107b58c0dd4e38327e95cf8b7d54ae12d
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-04-07 21:04:31 +0530

    user presence added

--- app/multiplay.tsx
+++ app/multiplay.tsx
@@ -2,38 +2,45 @@
 import React,{useRef,useEffect,useState} from 'react'
 import usePartySocket from "partysocket/react";
 
+type userListType = {
+  user:{
+    color:string,
+    pos:number
+  }
+}
+
 const multiplay = () => {
 
-    const [currentState,setCurrentState] = React.useState<string>('black')
+    const [currentState,setCurrentState] = React.useState<userListType>({user:{color:'0,0,0',pos:0}})
     const [index,setIndex] = React.useState<number>(0)
     const arr = new Array(10).fill(0)
 
     const socket = usePartySocket({
-        host: process.env.NEXT_PUBLIC_PARTYKIT_HOST,
+        host: process.env.NEXT_PUBLIC_PARTYKIT_HOST || 'http://127.0.0.1:1999',
         room: "123",
         onMessage(event) {
           const message = event.data;
           const parsedMsg = JSON.parse(message);
           if(parsedMsg.type == 'init'){
-            console.log(parsedMsg)
-            setCurrentState(parsedMsg.state.colour)
+            setCurrentState(parsedMsg.userList)
           }
         },
     });
-
+  
     const rollDice = () => {
+        console.log('roll dice')
         socket.send(JSON.stringify({ type: "roll-dice" }));
     };
 
   return (
     <div className='flex flex-col'>
-        <button onClick={()=>{rollDice}}>send event</button>
-        {currentState}
-        <div style={{background:currentState}} className={`w-10 h-10 bg-[${currentState}]`}>aaa</div>
+        <button className='bg-indigo-500' onClick={()=>{rollDice()}}>send event</button>
+        <div className={`w-10 h-10 bg-[${currentState}]`}>aaa</div>
         <h1>{index}</h1>
-        {/* {arr.map((e,i)=>{
-          return <div  className={`${index == i ? 'bg-red-600':'bg-green-500'}`} key={i}></div>
-        })} */}
+        {/* {console.log(Object.values(currentState))} */}
+        {Object.values(currentState).map((e:any,i)=>{
+          return <div style={{background:`rgb(${e.color})`}} key={i}>{e.color}--||--{e.pos}</div>
+        })}
     </div>
   )
 }
--- party/index.ts
+++ party/index.ts
@@ -2,47 +2,64 @@ import type * as Party from "partykit/server";
 
 const colr = () => Math.floor(Math.random()*255)
 
+interface MyObject {
+  t: {
+    color:string,
+    pos:number
+  };
+  [key: string]: {}; // Index signature
+}
+
 export default class Server implements Party.Server {
   constructor(readonly room: Party.Room) {
+       
+  }
+
+  async onClose(connection: Party.Connection<unknown>){
+    // connection.send()
+
+    let userList = await this.room.storage.get<MyObject>("userlist");
+    if(typeof userList == 'object' && userList){
+      delete userList[connection.id]
+    }
+    await this.room.storage.put("userlist",userList)
+    const event = new Event("message")
+    for (const everyone of this.room.getConnections()) {
+      everyone.send(JSON.stringify({type:'init',userList}));
+    }
     
   }
 
   async onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-      id: ${conn.id}
-      room: ${this.room.id}
-      url: ${new URL(ctx.request.url).pathname}`
-      );
     
-    conn.setState({colour:`rgb(${colr()},${colr()},${colr()})`})
-    // let's send a message to the connection
-    conn.send(JSON.stringify({type:'init',state:conn.state}));
+    let userList = await this.room.storage.get("userlist");
+    if(!userList){
+      await this.room.storage.put("userlist",{[conn.id]:{pos:0,color:`${colr()},${colr()},${colr()}`}})
+    }else {
+      userList = {...userList,[conn.id]:{pos:0,color:`${colr()},${colr()},${colr()}`}}
+      await this.room.storage.put("userlist",userList)
+    }
+    for (const everyone of this.room.getConnections()) {
+      everyone.send(JSON.stringify({type:'init',userList}));
+    }
   }
 
   async onMessage(message: string, sender: Party.Connection) {   
   
-    this.room.storage.put(`${sender.id}`,{position:0})
-    sender.setState({pos:0})
-    console.log(sender.state)
-
-    // let's log the message
-    // console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
+    // socket.send(JSON.stringify({ type: "roll-dice" })); 
     const postMessage = JSON.parse(message)
-    console.log(postMessage)
-    console.log(sender.state,'-state')
-    // this.room.broadcast(JSON.stringify({type:'init',state:sender.state}))
-    // dice event
-    // 
-
-    // this.room.broadcast(
-      // JSON.stringify(postMessage),
-      // `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      // [sender.id]
-    // );
+    if(postMessage.type == "roll-dice"){
+      let userList = await this.room.storage.get<MyObject>("userlist");
+      if(userList){
+        userList = {...userList,[sender.id]:{...userList[sender.id],pos:Math.floor(Math.random()*200)}}
+        await this.room.storage.put("userlist",userList)
+      }
+      console.log(sender.id,'sender id')
+      for (const everyone of this.room.getConnections()) {
+        everyone.send(JSON.stringify({type:'init',userList}));
+      }
+    }
+    console.log(postMessage,'-state')
   }
 }
 
